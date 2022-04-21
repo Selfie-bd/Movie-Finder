a6a2a39c64d8fc953daffc9757c203e92fa1b718
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-04-21 19:11:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Initializing the variables
+// Intizialising the variables
 let url = 'https://gist.githubusercontent.com/kevinnadar22/112e759f1f6f7747c5df8d8749e464f0/raw/0190049fe65baabc057662495125b0070cb6d764/data.json';
 
 
@@ -127,3 +127,32 @@ movieSubmit.addEventListener('click', displayMovie)
 
 // Reset Button
 reset.addEventListener('click', resetButton)
+
+
+// Scroll To Top
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
